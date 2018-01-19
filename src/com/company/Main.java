2ba1afe152b9b4a2f3945e818c6df8bf47b250b2
fedosovmu <fedosovmu@gmail.com>
2018-01-19 23:19:32 +0500

    RaceThreads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        // write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберите пример:");
+        System.out.println("1 Гонки потоков");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.print("пример:");
+        int mode = in.nextInt();
 
-class ThreadOne extends Thread {
-    @Override
-    public void run() {
-        System.out.println("thread One run");
-    }
-}
+        switch (mode) {
+            case 1:
+                RaceThreads.Start();
+                break;
+            case 2:
+            case 3:
+            default:
+                System.out.println("Недопустимое значение");
+                break;
+        }
 
-class ThreadTwo implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("thread Two run");
     }
 }
---
+++ src/com/company/RaceThreads.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class RaceThreads {
+    public static void Start() {
+        Resourse resourse = new Resourse();
+
+        ThreadOne one = new ThreadOne(resourse);
+        one.start();
+
+        ThreadTwo two = new ThreadTwo(resourse);
+        Thread thread2 = new Thread(two);
+        thread2.start();
+
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e) { }
+        System.out.println(resourse.Counter + " / " + 2000000);
+    }
+
+}
+
+class ThreadOne extends Thread {
+    private Resourse _resourse;
+
+    ThreadOne (Resourse ressourse) {
+        _resourse = ressourse;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("thread One run");
+        for (int i = 0; i < 1000000; i++) {
+            _resourse.Counter++;
+        }
+    }
+}
+
+class ThreadTwo implements Runnable {
+    Resourse _resourse;
+
+    ThreadTwo (Resourse resourse) {
+        _resourse = resourse;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("thread Two run");
+        for (int i = 0; i < 1000000; i++) {
+            _resourse.Counter++;
+        }
+    }
+}
+
+class Resourse {
+    public int Counter = 0;
+}
